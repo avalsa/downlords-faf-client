b1f2433ac451ff745699dfc6cab0e4661ece21a4
Aclrian <32142988+Aclrian@users.noreply.github.com>
2020-06-25 10:40:09 +0200

    spelling mistake

--- src/main/java/com/faforever/client/vault/search/SearchController.java
+++ src/main/java/com/faforever/client/vault/search/SearchController.java
@@ -223,7 +223,7 @@ public class SearchController implements Controller<Pane> {
     Optional<Condition> condition = initialSpecification.appendTo(qBuilder);
     String lastYearQuery = "";
     if(onlyShowLastYearCheckBox.isVisible() && onlyShowLastYearCheckBox.isSelected()){
-      lastYearQuery = generateOnlyLastYearQuerry();
+      lastYearQuery = generateOnlyLastYearQuery();
     }
 
     if (!condition.isPresent()) {
@@ -239,7 +239,7 @@ public class SearchController implements Controller<Pane> {
     return condition.get().query(new RSQLVisitor())+";"+lastYearQuery;
   }
 
-  private String generateOnlyLastYearQuerry(){
+  private String generateOnlyLastYearQuery(){
     Calendar calendar = Calendar.getInstance();
     calendar.add(Calendar.YEAR, -1);
     Date date = calendar.getTime();
